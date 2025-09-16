1c62cb4f34acde20d6ead766ad6f43620ea7e028
LeandroSearadaSilva <leandroseara3@hotmail.com>
2025-09-16 20:59:52 -0300

    Add files via upload

---
+++ Aula 04 19.09.2025/script.js
@@ -0,0 +1,28 @@
+const inputTexto = document.getElementById("texto");
+const button = document.getElementById("btn1");
+
+button.onclick = () => {
+    alert(`Eu digitei: ${inputTexto.value}`);
+   // alert(`Eu digitei: `+inputTexto.value)
+};
+const inputsenha = document.getElementById("senha");
+const button2 = document.getElementById("btn2");
+
+button2.onclick = () => {
+    alert(`Eu digitei: ${inputsenha.value}`);
+};
+//variavel
+let nome = maria
+let idade = 15
+let possui_carro = true
+
+//variavel objeto
+let pessoa = {
+    nome: "maria",
+    idade: 20,
+    possuiCarro: true
+}
+//variavel array
+
+    let listadFrutas = ["melancia", "pera", "maçã"]
+
